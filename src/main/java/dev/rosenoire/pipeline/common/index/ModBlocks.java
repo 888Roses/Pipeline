5f949345abe80ee73a0babf09ffd58a6ace2f17c
Rosenoire <skillwarzaking@gmail.com>
2026-02-05 17:29:19 +0100

    Pipes & Pipe Controllers now copy the properties of copper blocks.

--- src/main/java/dev/rosenoire/pipeline/common/index/ModBlocks.java
+++ src/main/java/dev/rosenoire/pipeline/common/index/ModBlocks.java
@@ -4,11 +4,13 @@ import dev.rosenoire.pipeline.common.block.CopperPipeBlock;
 import dev.rosenoire.pipeline.common.block.PipeControllerBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
 import static dev.rosenoire.pipeline.common.Pipeline.geode;
 
 public interface ModBlocks {
-    Block COPPER_PIPE = geode.registerBlock("copper_pipe", CopperPipeBlock::new, AbstractBlock.Settings.create().nonOpaque());
-    Block COPPER_PIPE_CONTROLLER = geode.registerBlock("copper_pipe_controller", PipeControllerBlock::new, AbstractBlock.Settings.create().nonOpaque());
+    Block COPPER_PIPE = geode.registerBlock("copper_pipe", CopperPipeBlock::new, AbstractBlock.Settings.copy(Blocks.COPPER_BLOCK).nonOpaque());
+    Block COPPER_PIPE_CONTROLLER = geode.registerBlock("copper_pipe_controller", PipeControllerBlock::new, AbstractBlock.Settings.copy(Blocks.COPPER_BLOCK).nonOpaque());
 
     static void register() {
     }
