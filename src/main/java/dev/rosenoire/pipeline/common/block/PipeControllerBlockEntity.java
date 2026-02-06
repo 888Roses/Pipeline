478fa81ab949a17f2629372f0c0e05ad2afc419b
Rosenoire <skillwarzaking@gmail.com>
2026-02-06 11:14:09 +0100

    Pipe controller can now filter tags.

--- src/main/java/dev/rosenoire/pipeline/common/block/PipeControllerBlockEntity.java
+++ src/main/java/dev/rosenoire/pipeline/common/block/PipeControllerBlockEntity.java
@@ -4,10 +4,17 @@ import dev.rosenoire.pipeline.common.index.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.entity.ContainerUser;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
 public class PipeControllerBlockEntity extends ChestBlockEntity {
@@ -22,7 +29,19 @@ public class PipeControllerBlockEntity extends ChestBlockEntity {
             return true;
         }
 
-        return isReverseMode != containsAny(otherStack -> ItemStack.areItemsAndComponentsEqual(stack, otherStack));
+        boolean isContained = containsAny(filterStack -> {
+            if (filterStack.isOf(Items.NAME_TAG) && filterStack.getCustomName() != null) {
+                String tagKeyName = filterStack.getCustomName().getString();
+                TagKey<Item> tag = TagKey.of(RegistryKeys.ITEM, Identifier.of(tagKeyName));
+                if (stack.isIn(tag)) {
+                    return true;
+                }
+            }
+
+            return ItemStack.areItemsAndComponentsEqual(stack, filterStack);
+        });
+
+        return isReverseMode != isContained;
     }
 
     @Override
