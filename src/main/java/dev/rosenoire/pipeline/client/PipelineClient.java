a54d9cba4aeaf24480f020adb66be2c874f4512f
Rosenoire <skillwarzaking@gmail.com>
2026-02-06 10:05:53 +0100

    Added the copper pipe & pipe controller to the redstone creative tab.

--- src/main/java/dev/rosenoire/pipeline/client/PipelineClient.java
+++ src/main/java/dev/rosenoire/pipeline/client/PipelineClient.java
@@ -3,8 +3,12 @@ package dev.rosenoire.pipeline.client;
 import dev.rosenoire.pipeline.client.index.ModBlockEntityRenderers;
 import dev.rosenoire.pipeline.client.index.ModTooltipComponents;
 import dev.rosenoire.pipeline.common.Pipeline;
+import dev.rosenoire.pipeline.common.index.ModItems;
 import net.collectively.geode.GeodeClient;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 
 public class PipelineClient implements ClientModInitializer {
     public static final GeodeClient geode = GeodeClient.create(Pipeline.geode.getLinkedModId());
@@ -15,6 +19,10 @@ public class PipelineClient implements ClientModInitializer {
 
         ModBlockEntityRenderers.register();
         ModTooltipComponents.register();
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> {
+            entries.addAfter(Items.WAXED_OXIDIZED_COPPER_BULB, ModItems.COPPER_PIPE, ModItems.COPPER_PIPE_CONTROLLER);
+        });
 
         geode.register();
     }
